ae10e6e7fb6c5c1b5dc81f5136f654cf931afee1
Valerie <valerie@segebre.org>
2016-11-19 01:43:34 +0000

    Lesson 7 final commit

---
+++ Lesson7_Assignment/script.js
@@ -0,0 +1,21 @@
+/*
+    Program Name: Recipe Display Application
+    Author: Valerie Segebre
+    Date: November 18, 2016
+    Filename: script.js
+*/
+
+    //displays the next element after the current target
+    function display(event) {
+        $(event.currentTarget).next().fadeIn(“slow”);
+    }//end of display
+    
+    //attach event listener to h3 elements to invoke display function when clicked
+    $("h3").click(display);
+    
+    
+    
+    
+    
+    
+    
